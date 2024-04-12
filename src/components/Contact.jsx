9a8cf09a3dce0c2dbdd32a9206ba3cef726e0c7b
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-04-12 16:22:10 +0200

    clone

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,9 +4,8 @@ import { CiEdit } from "react-icons/ci";
 import { MdDelete } from "react-icons/md";
 const Contact = () => {
     return (
-    
-            <div className='bg-black py-7 px-7 mb-3  flex flex-row gap-32'>
-            <div className=''>
+          <section className='bg-black py-7 px-7 mb-3  flex flex-row gap-32'>
+            <div>
              <h3>Name: {Contact.fullName}</h3>
              <h3>Phone:{Contact.phone}</h3>
              <h3>Email: {Contact.email}</h3>
@@ -16,14 +15,14 @@ const Contact = () => {
               <Link to={'`${contact._id}`'} className=''><FaEye /></Link>
               </div>
               <div className=''>
-              <Link to={'/update/:contactId'} className=''><CiEdit /></Link>
+              <Link to={'`${/update/:contactId}`'} className=''><CiEdit /></Link>
               </div>
               <div className=''>
               <Link to={'/deleteContact'} className=''><MdDelete /></Link>
               </div>
             </div>  
               
-         </div> 
+         </section> 
                         
     
     )
