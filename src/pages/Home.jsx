298984efafdd83ba963c35b56dbb0639fa5c6699
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-04-13 11:57:11 +0200

    checking grids

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,10 +29,11 @@ const Home = () => {
             </p>
           </div>
 
-          
+          <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-2">
+            {contacts.map((contact) => (
               <Contact />
-          
-          
+            ))}
+          </div>
         </div>
       </section>
     </div>
