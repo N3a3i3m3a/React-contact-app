50dac169acbc0bc7b399921e46769c98b4ddccdb
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-04-15 09:35:54 +0200

    fetch ref

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,9 +30,10 @@ const Home = () => {
           </div>
           
           <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-2">
-            {contacts.map((contact) => (
+            <Contact/>
+            {/*{contacts.map((contact) => (
               <Contact key={contact._id} contact={contact} />
-            ))}
+            ))} */} 
           </div>
         </div>
       </section>
